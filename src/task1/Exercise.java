6ca77c20ebe2f4e9b9748db13ff898f1780668bf
Denis Pitsul <denia.pitsul@gmail.com>
2024-02-16 21:05:07 +0200

    Initial commit

---
+++ src/task1/Exercise.java
@@ -0,0 +1,27 @@
+package task1;
+
+public class Output {
+
+    public void sayHelloIfNumberGraterThan7(int number) {
+        if (number > 7) {
+            System.out.println("Hello");
+        }
+    }
+
+    public void sayHelloJohnOrOutputMassage(String name) {
+        if (name.equals("John")) {
+            System.out.println("Hello, John");
+        } else {
+            System.out.println("There is no such name");
+        }
+    }
+
+    public void outputArrayElementsThatAreMultiplesOfNumber3(int[] array) {
+        System.out.println("Array elements that are multiples of 3:");
+        for (int num : array) {
+            if (num % 3 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
